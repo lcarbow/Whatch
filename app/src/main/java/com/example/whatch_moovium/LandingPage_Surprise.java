2041ce00aa3370904073401bc8ada7a38ca37d04
mlamowski <82083936+mlamowski@users.noreply.github.com>
2022-10-13 20:11:36 +0200

    create new activty

---
+++ app/src/main/java/com/example/whatch_moovium/LandingPage_Surprise.java
@@ -0,0 +1,14 @@
+package com.example.whatch_moovium;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LandingPage_Surprise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page_surprise);
+    }
+}
