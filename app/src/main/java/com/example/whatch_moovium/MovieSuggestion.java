941e54b7f354ae231e14b56d44558c06900bdc9a
mlamowski <82083936+mlamowski@users.noreply.github.com>
2022-10-14 12:55:08 +0200

    landing

--- app/src/main/java/com/example/whatch_moovium/MovieSuggestion.java
+++ app/src/main/java/com/example/whatch_moovium/MovieSuggestion.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TestActivty extends AppCompatActivity {
+public class MovieSuggestion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_activty);
+        setContentView(R.layout.activity_movie_suggestion);
     }
 }
