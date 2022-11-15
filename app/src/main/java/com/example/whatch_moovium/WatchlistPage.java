1a5a808520ec26429aa8026e80ce7b400697f659
Nadine <Nadine.Friede@haw-hamburg.de>
2022-11-15 13:50:29 +0100

    Buttons fuer Watchlist und Provider und Icons der BottomNav groeßer gemacht

--- app/src/main/java/com/example/whatch_moovium/WatchlistPage.java
+++ app/src/main/java/com/example/whatch_moovium/WatchlistPage.java
@@ -3,46 +3,25 @@ package com.example.whatch_moovium;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class LandingPage_Surprise extends AppCompatActivity implements Contract.LandingView {
+public class WatchlistPage extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing_page_surprise);
+        setContentView(R.layout.activity_watchlist_page);
 
-        //Views
-        Button button = findViewById(R.id.button);
-
-        //Presenter
-        Contract.Presenter presenter;
-        presenter = new Presenter(this);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                presenter.onButtonClick();
-
-            }
-        });
-
-//Bottom Nav
+        //Bottom Nav
         bottomNavigationView = findViewById(R.id.bottom_navigator);
-        bottomNavigationView.setSelectedItemId(R.id.surprise);
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -50,6 +29,8 @@ public class LandingPage_Surprise extends AppCompatActivity implements Contract.
                 switch(item.getItemId())
                 {
                     case R.id.surprise:
+                        startActivity(new Intent(getApplicationContext(),LandingPage_Surprise.class));
+                        overridePendingTransition(0,0);
                         return true;
                     case R.id.mood:
                         startActivity(new Intent(getApplicationContext(),LandingPage_Mood.class));
@@ -66,26 +47,9 @@ public class LandingPage_Surprise extends AppCompatActivity implements Contract.
 
 //Top Nav
         ImageButton providersButton = findViewById(R.id.providers_button);
-        ImageButton watchlistButton = findViewById(R.id.watchlist_button);
+        //ImageButton watchlistButton = findViewById(R.id.watchlist_button);
 
         providersButton.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(),ProviderSettings.class)));
-        watchlistButton.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(),WatchlistPage.class)));
-
-
+        //watchlistButton.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(),WatchlistPage.class)));
     }
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        SharedPreferences prefs = getSharedPreferences("X", MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString("lastActivity", getClass().getName());
-        editor.commit();
-    }
-
-    @Override
-    public Context getContext() {
-        return LandingPage_Surprise.this;
-    }
-
 }
